3290319046d1aa67e1ec2f5e8b7d9ebd0c44462d
Srinivas Eathakota <seathakota@guidewire.com>
2022-03-03 13:35:33 -0600

    Update Menu Item llist and Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
-import  HomePage  from './pages/homepage/homepage.component';
+
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className='App'>
-   <HomePage />
+    <div>
+   <Router>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
+      </Router>
     </div>
   );
 }
