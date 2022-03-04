b4edf5296f75e368419f704d1d9980dcf579cfcb
Srinivas Eathakota <seathakota@guidewire.com>
2022-03-04 12:40:57 -0600

    Commintig Changes to Sign in form and cutom button components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/Sign-in-and-Sign-UpPage/Sign-in-and-Sign-Up.componnet';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
@@ -17,6 +18,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </Router>
     </div>
